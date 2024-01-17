24e9caf2c00ecbb37ae41fb18692ce0dd9838951
Samarendra Gouda <samarendragouda@gmail.com>
2024-01-17 23:09:36 +0530

    feat: minor-fix

--- pages/proposals/index.vue
+++ pages/proposals/index.vue
@@ -38,7 +38,7 @@
       v-model="isOpen"
       :ui="{
         strategy: 'override',
-        base: 'sm:max-w-5xl w-5xl',
+        width: 'sm:max-w-4xl',
         overlay: {
           background: 'bg-gray-200/75 dark:bg-gray-800/75 backdrop-blur-lg',
         },
